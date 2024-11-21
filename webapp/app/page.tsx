4dece6b3580bc8460a49b66d40800ca776c0e20e
loris imbrogno <143715480+Loris-imbrogno@users.noreply.github.com>
2024-11-21 10:25:34 +0100

    small enter button to get onto the site

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,9 +1,13 @@
-export default function Home() {
+import Link from 'next/link';
 
+export default function Home() {
   return (
     <main>
       <div>
         <p>haiii</p>
+        <Link href="/dashboard/registration">
+          <button>Enter</button>
+        </Link>
       </div>
     </main>
   );
