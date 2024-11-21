2106ed05a9a22bcbd793032a3ebff47ed0f44974
jfuerst <jvfuerst01@gmail.com>
2024-11-21 11:29:23 +0100

    sort first leaderboard by speed and removed sign out button

--- webapp/app/dashboard/leaderboard/page.tsx
+++ webapp/app/dashboard/leaderboard/page.tsx
@@ -3,11 +3,12 @@ import { fetchRuns } from "@/app/lib/actions/runs/data";
 
 export default async function Page() {
 const data = await fetchRuns();
+
     return (
     <div>
       <div>
       <h2>Top speed</h2>
-      <LeaderboardTable run={data}/>
+      <LeaderboardTable run={data.sort((a, b) => a.duration! - b.duration!)}/>
       </div>
       <div>
       <h2>Top Consistency</h2>
