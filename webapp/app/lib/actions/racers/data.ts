59b2347399cb0c1aade999e7a2cb96d680e7bf98
jfuerst <jvfuerst01@gmail.com>
2024-11-28 13:50:49 +0100

    Add API layer for runs and racers

--- webapp/app/lib/actions/racers/data.ts
+++ webapp/app/lib/actions/racers/data.ts
@@ -3,6 +3,7 @@ import { racer } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// Fetch by Id
 export async function fetchRacerById(racer_id: string) {
   const racer = await prisma.racer.findUnique({
     where: { racer_id },
@@ -15,14 +16,41 @@ export async function fetchRacerById(racer_id: string) {
   return racer;
 }
 
+// Fetch all racers
 export async function fetchRacers() {
   const racers = await prisma.racer.findMany();
   return racers;
 }
 
-export async function createRacer(racer: racer) {
-  const newRacer = await prisma.racer.create({ data: racer });
+//Update racer or create racer if record doesnt exist yet
+export async function updateOrCreateRacer(racer: racer) {
+  const newRacer = await prisma.racer.upsert({ 
+    where: {
+      racer_id: racer.racer_id,
+    },
+    update: {
+      racer_id: racer.racer_id,
+      ldap: racer.ldap,
+      name: racer.name,
+      ski_passes: racer.ski_passes,
+    },
+    create: {
+      racer_id: racer.racer_id,
+      ldap: racer.ldap,
+      name: racer.name,
+      ski_passes: racer.ski_passes,
+    },
+   });
   return newRacer;
+}
+
+//Delete racer
+export async function deleteRacer(racer: racer){
+  const deleteRacer = await prisma.racer.delete({
+    where: {
+      racer_id: racer.racer_id
+    }
+  }).then((res) => console.log(res))
 }
 
 // Fetch recent runs for a given racer
---
+++ webapp/app/pages/api/racers/racer.ts
@@ -0,0 +1,34 @@
+import { deleteRacer, fetchRacerById, fetchRacers, updateOrCreateRacer } from '@/app/lib/actions/racers/data';
+import { racer } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const data : racer = JSON.parse(req.body)
+
+  switch(req.method) { 
+    case 'GET': { 
+       if(req.body == null){
+        res.json(fetchRacers())
+       } else {
+        res.json(fetchRacerById(data.racer_id))
+       }
+       break; 
+    } 
+    case 'POST': { 
+       res.json(updateOrCreateRacer(data))
+       break; 
+    } 
+    case 'PUT': { 
+        res.json(updateOrCreateRacer(data))
+        break; 
+     } 
+     case 'DELETE': { 
+        res.json(deleteRacer(data))
+        break; 
+     } 
+    default: { 
+       res.status(200).json({message: 'You made it to the racer API! nice.'})
+       break; 
+    } 
+ } 
+}
