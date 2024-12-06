d5b0255dfb2c9618d9066ffaf98d6bf84061e892
battino <battino46@gmail.com>
2024-12-06 14:37:54 +0000

    rebuilt alpenhunde class

--- statemachine/alpenhunde.py
+++ statemachine/alpenhunde.py
@@ -1,23 +1,20 @@
 import requests
-
-class Alpenhunde:
-    def __init__(self):
+from threading import Event
+from global_types import StateMachine, StateMachineState
+class Alpenhunde():
+    def __init__(self, update_event: Event, global_state: StateMachine):
         self.race_running = False
-        self.base_index = None
-        self.previous_highest_index = None
-        self.prev_len = None
-        self.last_time = None
+        self.update_event = update_event
+        self.global_state = global_state
+        self.prev_result_len = None
+        
+        
 
-        times = self.get_race_results()["times"]
-        self.previous_highest_index = self.get_highest_timing_index(times)
-        self.prev_len = len(times)
-        self.race_running = self.is_race_running()
-
-    def get_race_status(self):
+    def get_running_races(self):
         try:
             response = requests.get("http://192.168.4.1/timing/?action=get_name_queue_and_running_times")
             response.raise_for_status()
-            return response.json()    
+            return response.json()["times"]
         except requests.RequestException as e:
             print(f"Error fetching race status: {e}")
             return None
@@ -26,60 +23,51 @@ class Alpenhunde:
         try:
             response = requests.get("http://192.168.4.1/timing/results/")
             response.raise_for_status()
-            return response.json()
+            return response.json()["times"]
         except requests.RequestException as e:
             print(f"Error fetching race results: {e}")
             return None
 
+
     def is_race_running(self):
-        response = self.get_race_status()
-        return int(response["times"][0]["index"]) != 65535
+        response = self.get_running_races()
+        return int(response[0]["index"]) != 65535
 
-    def get_highest_timing_index(self, times):
-        highest_index_entry = max(times, key=lambda x: int(x["i"]))
-        return int(highest_index_entry["i"])
-
-    def send_post_request(self, url):
+    def stop_race(self,index):
         try:
-            response = requests.post(url)
-            response.raise_for_status()
+            response = requests.post("http://192.168.4.1/timing/?action=cancel&index=" + index)
         except requests.RequestException as e:
             print(f"Error sending POST request: {e}")
 
-    def stop_all_running(self):
-        if self.is_race_running():
-            currently_running = self.get_race_status()["times"]
-            for curr in currently_running:
-                self.send_post_request("http://192.168.4.1/timing/?action=cancel&index=" + curr["index"])
-
     def run(self):
-        try:
-            self.race_running = self.is_race_running()
-            print(f"Race running: {self.race_running}")
+        while True:
+            self.update_event.wait()
+            self.update_event.clear()
+            match self.global_state.current_state:
+                case StateMachineState.IDLE:
+                    self.clear()
+                case _:
+                    self.update()
 
-            times = self.get_race_results()["times"]
+    def update(self):
+        running = self.is_race_running()
+        result_timings = self.get_race_results()
+        if len(result_timings) != self.prev_result_len:
+            self.prev_result_len = len(result_timings)
+            new_time = result_timings[len(result_timings)-1]["t"]
+            if int(new_time) > 0:
+                print(f"New time: {new_time}")
+                self.global_state.last_race_time = new_time
+                self.clear()
+                
+        if (running):
+            self.global_state.current_state = StateMachineState.RUNNING
 
-            highest_index = self.get_highest_timing_index(times)
-            print(f"Highest index: {highest_index-1}")
-
-            if len(times) != self.prev_len:
-                print("New entry detected")
-                new_time = times[len(times)-1]["t"]
-                self.prev_len = len(times)
-                if int(new_time) > 0:
-                    print("times: ", new_time)
-                    self.last_time = new_time
-
-            if highest_index > self.previous_highest_index and self.race_running:
-                print("stopping all running")
-                self.stop_all_running()
-                self.prev_len = len(self.get_race_results()["times"])
-
-            self.previous_highest_index = highest_index
-
-        except Exception as e:
-            print(f"Flo made an oopsie: {e}")
-
-# Example usage:
-# alpenhunde = Alpenhunde()
-# alpenhunde.run()
+        else:
+            self.global_state.current_state = StateMachineState.IDLE
+            
+    def clear(self):
+        if self.is_race_running():
+            currently_running = self.get_running_races()
+            for curr in currently_running:
+                self.stop_race(curr["index"])
