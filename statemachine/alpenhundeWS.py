38f4b79ab72e868c653ff38cfa5d82c16f19da30
battino <battino46@gmail.com>
2024-12-06 14:38:11 +0000

    abstracted websocets to a class

---
+++ statemachine/alpenhundeWS.py
@@ -0,0 +1,33 @@
+import websocket
+import time
+from queue import Queue
+
+class AlpenhundeWS:
+    def __init__(self, message_queue: Queue):
+        self.message_queue = message_queue
+
+    def on_error(self, ws, error):
+        self.message_queue.put("WSError")
+
+    def on_close(self, ws, close_status_code, close_msg):
+        self.message_queue.put("WSClosed")
+        # Attempt to reconnect
+        time.sleep(5)
+        self.connect_websocket()
+
+    def on_open(self, ws):
+        print("WebSocket connection opened")
+
+    def on_message(self, ws, message):
+        self.message_queue.put("UpdateAlpenhunde")
+
+    def connect_websocket(self):
+        ws = websocket.WebSocketApp(
+            "ws://192.168.4.1/ws/events",
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close,
+            on_open=self.on_open
+        )
+        self.message_queue.put("WSConnected")
+        ws.run_forever(ping_interval=3, ping_timeout=1)
