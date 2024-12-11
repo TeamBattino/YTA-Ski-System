fea75ae0dda15f80f643a544ef5006b7f8fe89d7
battino <battino46@gmail.com>
2024-12-11 15:20:36 +0000

    add User to statemachine

--- statemachine/global_types.py
+++ statemachine/global_types.py
@@ -6,7 +6,14 @@ class StateMachineState(Enum):
     REGISTERED = 2
     RUNNING = 3
 
+
+class User:
+    rfid: str
+    name: str
+
 @dataclass
 class StateMachine:
     current_state: StateMachineState
     last_race_time: int
+    user: User
+    loading: bool
