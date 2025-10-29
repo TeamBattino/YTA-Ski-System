b63adc2bc2881223cf79a7ab87ccee362131bd5d
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2025-10-29 20:18:27 +0100

    Revert "delete unused files"
    This reverts commit a90f5e2f4da519d228d574bd050febdbc218aeef.

---
+++ webapp/auth.ts
@@ -0,0 +1,65 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+ 
+async function getAdmin(admin_username: string){
+  console.log('trying to get admin');
+  try{
+    const admin = await prisma.admin.findUnique({
+      where: { username: admin_username },
+    });
+
+    if (!admin) {
+      console.log('Admin not found');
+      return null;
+    }
+
+    return {
+      id: '', // even if admin model doesnt have id, authorize function wont work without it
+      username: admin?.username,
+      password: admin?.password
+    };
+  } catch (error){
+    console.log('couldnt fetch admin')
+    return null;
+  }
+}
+ 
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        if (!credentials) {
+          console.log('No credentials provided');
+          return null;
+        }
+        console.log(credentials)
+
+        const parsedCredentials = z
+          .object({ username: z.string(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+          console.log(parsedCredentials);
+
+          
+ 
+        if (parsedCredentials.success) {
+          const { username, password } = parsedCredentials.data;
+          const admin = await getAdmin(username);
+          if (!admin) return null;
+          const passwordsMatch = (admin.password === password);
+          if(passwordsMatch){
+            return admin;
+          }
+        }
+        console.log('Invalid credentials, sent from auth.ts');
+        return null;
+      },
+    }),
+  ],
+});
