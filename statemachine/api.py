4ba6fdf44e1938a32c764483be43d2d61248c63f
BusyBusinessPartner <jan.ludwig@edu.tbz.ch>
2025-11-20 12:07:23 +0100

    Add race_id to API Client.

--- statemachine/api.py
+++ statemachine/api.py
@@ -5,14 +5,15 @@ import os
 class ApiClient:
     def __init__(self, base_url):
         self.base_url = base_url
+        self.race_id = "6d3173f4-823b-4e05-a4d7-b2decacc7ade"
 
     def getUser(self, ski_pass) -> User:
         url = f"https://{self.base_url}/api/racers"
-        params = {'ski_pass': ski_pass}
+        params = {'ski_pass': ski_pass, 'race_id': self.race_id}
         response = requests.get(url, params=params)
         if response.status_code == 404:
             print(f"User {ski_pass} not found")
-            return User("Unregistered User","Unregistered User")
+            return User("Unregistered User","Unregistered User", "Unregistered User")
         req_user = response.json()
         print(req_user)
         if response.status_code == 200:
@@ -23,7 +24,7 @@ class ApiClient:
             return
         print(f"Posting {ski_pass} with duration {duration}")
         url = f"https://{self.base_url}/api/runs"
-        json_data = {'ski_pass': ski_pass, 'duration': int(duration)}
+        json_data = {'ski_pass': ski_pass, 'duration': int(duration), 'race_id': self.race_id}
         headers = {'secret-key': os.getenv('AUTH_SECRET')}
         print(json_data)
         response = requests.post(url, json=json_data, headers=headers)
