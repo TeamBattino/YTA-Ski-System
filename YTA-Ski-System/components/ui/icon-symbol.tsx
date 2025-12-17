055aa79a95517b3b9fa1a07d086a2fdf08eae8f1
BusyBusinessPartner <jan.ludwig@edu.tbz.ch>
2025-12-17 14:58:00 +0100

    Add Icons for tab bar

--- YTA-Ski-System/components/ui/icon-symbol.tsx
+++ YTA-Ski-System/components/ui/icon-symbol.tsx
@@ -18,6 +18,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'person.badge.plus': 'person-add',
+  'trophy.fill': 'emoji-events',
 } as IconMapping;
 
 /**
