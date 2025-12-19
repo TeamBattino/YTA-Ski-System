f4726e413a5948453d0498d1af285da74fa818a1
BusyBusinessPartner <jan.ludwig@edu.tbz.ch>
2025-12-19 16:29:56 +0100

    Update incorrect Path

--- statemachine/run.sh
+++ statemachine/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/bin/unclutter -idle 0.1 -root &
-source /home/pi/Documents/YTA-Ski-System/statemachine/.venv/bin/activate
-/home/pi/Documents/YTA-Ski-System/statemachine/.venv/bin/python3 /home/pi/Documents/YTA-Ski-System/statemachine/main.py
+source /home/pi/YTA-Ski-System/statemachine/.venv/bin/activate
+/home/pi/YTA-Ski-System/statemachine/.venv/bin/python3 /home/pi/YTA-Ski-System/statemachine/main.py
